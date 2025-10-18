93d6eaf1eee8ac43d5db1bcaa12e034ae0aec261
Gedan Magalhães <gedan.magalhaes@xjur.com.br>
2025-10-18 09:27:45 -0300

    :white_check_mark: include tests

---
+++ internal/adapter/database/model/session_model_test.go
@@ -0,0 +1,51 @@
+package model
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+)
+
+func TestSessionModelInitialization(t *testing.T) {
+	refreshToken := "some-refresh-token"
+	s := SessionModel{
+		UserID:       1,
+		RefreshToken: &refreshToken,
+	}
+
+	assert.NotNil(t, s)
+	assert.Equal(t, uint(1), s.UserID)
+	assert.Equal(t, &refreshToken, s.RefreshToken)
+}
+
+func TestSessionModel_ToFromDomain(t *testing.T) {
+	now := time.Now()
+	deletedAt := now.Add(time.Hour * 1)
+	refreshToken := "some-refresh-token"
+
+	modelSession := &SessionModel{
+		Model: gorm.Model{
+			ID:        1,
+			CreatedAt: now,
+			UpdatedAt: now,
+			DeletedAt: gorm.DeletedAt{Time: deletedAt, Valid: true},
+		},
+		UserID:       1,
+		RefreshToken: &refreshToken,
+	}
+
+	domainSession := modelSession.ToDomain()
+
+	assert.Equal(t, modelSession.ID, domainSession.ID)
+	assert.Equal(t, modelSession.UserID, domainSession.UserID)
+	assert.Equal(t, modelSession.RefreshToken, domainSession.RefreshToken)
+	assert.Equal(t, modelSession.CreatedAt, domainSession.CreatedAt)
+	assert.Equal(t, modelSession.UpdatedAt, domainSession.UpdatedAt)
+
+	newModel := FromDomainSession(domainSession)
+	newModel.DeletedAt = modelSession.DeletedAt
+
+	assert.Equal(t, modelSession, newModel)
+}
