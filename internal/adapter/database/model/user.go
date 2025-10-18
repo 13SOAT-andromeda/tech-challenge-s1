be3d65cf1c5a595e45df6aaa7ae3c292d280e0e4
Bruno Araujo <bruno.ar.avl@gmail.com>
2025-10-17 23:59:37 -0300

    feat: userModel done

---
+++ internal/adapter/database/model/user.go
@@ -0,0 +1,46 @@
+package model
+
+import "github.com/13SOAT-andromeda/tech-challenge-s1/internal/domain"
+
+type UserModel struct {
+	ID       uint         `gorm:"primaryKey"`
+	Name     string       `gorm:"not null"`
+	Email    string       `gorm:"not null"`
+	Contact  string       `gorm:"not null"`
+	Address  AddressModel `gorm:"embedded"`
+	Password string       `gorm:"not null"`
+	Role     string       `gorm:"not null"`
+}
+
+func (u *UserModel) TableName() string {
+	return "Users"
+}
+
+func NewUserModelFromDomain(domain domain.User) UserModel {
+	return UserModel{
+		ID:       domain.ID,
+		Name:     domain.Name,
+		Email:    domain.Email,
+		Contact:  domain.Contact,
+		Role:     domain.Role,
+		Password: domain.Password.Get(),
+		Address:  FromDomainAddress(domain.Address),
+	}
+}
+
+func (u *UserModel) ToDomain() domain.User {
+	pass, err := domain.NewPassword(u.Password)
+
+	if err != nil {
+		return domain.User{}
+	}
+
+	return domain.User{
+		ID:       u.ID,
+		Name:     u.Name,
+		Email:    u.Email,
+		Contact:  u.Contact,
+		Role:     u.Role,
+		Password: pass,
+	}
+}
