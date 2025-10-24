476721c74b42374869065a49c61ed939ec4954b0
Gedan Magalhães <gedan.magalhaes@xjur.com.br>
2025-10-24 01:37:35 -0300

    :art: add parameter to include deleted data query

--- internal/application/ports/base_repository.go
+++ internal/application/ports/base_repository.go
@@ -4,7 +4,7 @@ import "context"
 
 type Repository[T any] interface {
 	FindByID(ctx context.Context, id uint) (*T, error)
-	FindAll(ctx context.Context) ([]T, error)
+	FindAll(ctx context.Context, includeDeleted bool) ([]T, error)
 	Create(ctx context.Context, entity *T) (*T, error)
 	Update(ctx context.Context, entity *T) error
 	Delete(ctx context.Context, id uint) error
