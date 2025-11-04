951e4c129a97956458024640ce4be633c1068c5f
Bruno Araujo <bruno.ar.avl@gmail.com>
2025-11-04 03:27:33 -0300

    feat: functions to generate valid plate and valid CPF

--- test/e2e/helper.go
+++ test/e2e/helper.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"strconv"
 
 	"github.com/13SOAT-andromeda/tech-challenge-s1/internal/adapter/config"
 )
@@ -102,4 +103,88 @@ func BuildBody[T any](data T) (io.Reader, error) {
 		return nil, err
 	}
 	return bytes.NewBuffer(jsonData), nil
+}
+
+func GenerateValidCPF(seed int64) string {
+	base := seed % 1000000000
+	if base < 100000000 {
+		base += 100000000
+	}
+
+	baseStr := strconv.FormatInt(base, 10)
+
+	firstChar := baseStr[0]
+	allSame := true
+	for i := 1; i < len(baseStr); i++ {
+		if baseStr[i] != firstChar {
+			allSame = false
+			break
+		}
+	}
+
+	if allSame {
+		lastDigit, _ := strconv.Atoi(string(baseStr[8]))
+		lastDigit = (lastDigit + 1) % 10
+		baseStr = baseStr[:8] + strconv.Itoa(lastDigit)
+	}
+
+	sum := 0
+	for i := 0; i < 9; i++ {
+		digit, _ := strconv.Atoi(string(baseStr[i]))
+		sum += digit * (10 - i)
+	}
+
+	firstDigit := (sum * 10) % 11
+	if firstDigit == 10 {
+		firstDigit = 0
+	}
+
+	baseStr += strconv.Itoa(firstDigit)
+	sum = 0
+	for i := 0; i < 10; i++ {
+		digit, _ := strconv.Atoi(string(baseStr[i]))
+		sum += digit * (11 - i)
+	}
+
+	secondDigit := (sum * 10) % 11
+	if secondDigit == 10 {
+		secondDigit = 0
+	}
+
+	cpf := baseStr + strconv.Itoa(secondDigit)
+
+	firstChar = cpf[0]
+	allSame = true
+	for i := 1; i < len(cpf); i++ {
+		if cpf[i] != firstChar {
+			allSame = false
+			break
+		}
+	}
+
+	if allSame {
+		return GenerateValidCPF(seed + 1)
+	}
+
+	return cpf
+}
+
+func GenerateValidPlate(seed int64) string {
+	letters := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	letter1 := letters[seed%26]
+	letter2 := letters[(seed/26)%26]
+	letter3 := letters[(seed/676)%26]
+
+	numbers := seed % 10000
+	if numbers < 0 {
+		numbers = -numbers
+	}
+
+	numbersStr := strconv.FormatInt(numbers, 10)
+	for len(numbersStr) < 4 {
+		numbersStr = "0" + numbersStr
+	}
+
+	return string(letter1) + string(letter2) + string(letter3) + numbersStr
 }
