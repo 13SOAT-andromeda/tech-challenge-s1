80fdfcf6a9a17ace68d6f43d921985da6f1db98e
Bruno Araujo <bruno.ar.avl@gmail.com>
2025-11-03 23:11:04 -0300

    feat: session e2e completed

--- test/e2e/session_test.go
+++ test/e2e/session_test.go
@@ -140,6 +140,184 @@ func TestSession(t *testing.T) {
 		assert.NotEmpty(t, baseResponseValidate.Data.User.Role)
 		assert.True(t, baseResponseValidate.Data.User.Active)
 	})
+
+	t.Run("should invalid token return an error", func(t *testing.T) {
+		var baseResponseValidate response.BaseResponse[any]
+
+		resp, err := NewAuthenticatedReq("GET", apiUrl+"/sessions/validate", nil, "invalidtoken")
+
+		require.NoError(t, err, "Failed on make a validate request")
+
+		bodyBytes, err := io.ReadAll(resp.Body)
+
+		require.NoError(t, err, "Failed to parse validate body response")
+
+		defer resp.Body.Close()
+
+		err = ParseBody(bodyBytes, &baseResponseValidate)
+
+		require.NoError(t, err, "Failed to parse validate response body")
+
+		assert.Equal(t, resp.StatusCode, http.StatusInternalServerError)
+		assert.False(t, baseResponseValidate.Success)
+		assert.Contains(t, baseResponseValidate.Message, "token is malformed")
+	})
+
+	t.Run("should refresh token route results in successful", func(t *testing.T) {
+		var loginData handlers.LoginRequest
+		var refreshData handlers.RefreshRequest
+		var baseResponse response.BaseResponse[session.LoginOutput]
+		var baseResponseRefresh response.BaseResponse[session.RefreshOutput]
+
+		loginData.Email = cfg.AdminUser.Email
+		loginData.Password = cfg.AdminUser.Password
+
+		bodyBytes, resp := loginRequest(t, loginData, apiUrl)
+
+		err = ParseBody(bodyBytes, &baseResponse)
+
+		require.NoError(t, err, "Failed to parse response body")
+
+		assert.Equal(t, resp.StatusCode, http.StatusOK)
+		assert.True(t, baseResponse.Success)
+		assert.NotEmpty(t, baseResponse.Data.AccessToken)
+		assert.NotEmpty(t, baseResponse.Data.RefreshToken)
+		assert.NotEmpty(t, baseResponse.Data.ExpiresIn)
+		assert.NotEmpty(t, baseResponse.Data.User)
+		assert.NotNil(t, baseResponse.Data.User)
+		assert.NotZero(t, baseResponse.Data.User.ID)
+		assert.Equal(t, cfg.AdminUser.Email, baseResponse.Data.User.Email)
+		assert.NotEmpty(t, baseResponse.Data.User.Role)
+		assert.True(t, baseResponse.Data.User.Active)
+
+		refreshData.RefreshToken = baseResponse.Data.RefreshToken
+
+		payload, err := BuildBody(refreshData)
+
+		require.NoError(t, err, "Failed to build refresh request")
+
+		resp, err = NewUnauthenticatedReq("POST", apiUrl+"/sessions/refresh", payload)
+
+		require.NoError(t, err, "Failed on make a refresh request")
+
+		bodyBytes, err = io.ReadAll(resp.Body)
+
+		require.NoError(t, err, "Failed to parse refresh body response")
+
+		defer resp.Body.Close()
+
+		err = ParseBody(bodyBytes, &baseResponseRefresh)
+
+		require.NoError(t, err, "Failed to parse refresh response body")
+
+		assert.Equal(t, resp.StatusCode, http.StatusOK)
+		assert.True(t, baseResponseRefresh.Success)
+		assert.NotEmpty(t, baseResponseRefresh.Data.AccessToken)
+		assert.NotEmpty(t, baseResponseRefresh.Data.RefreshToken)
+		assert.NotZero(t, baseResponseRefresh.Data.ExpiresIn)
+	})
+
+	t.Run("should invalid refresh token return an error", func(t *testing.T) {
+		var baseResponseRefresh response.BaseResponse[any]
+		var refreshData handlers.RefreshRequest
+
+		refreshData.RefreshToken = "invalidtoken"
+
+		body, err := BuildBody(refreshData)
+
+		require.NoError(t, err, "Failed to build refresh validate body")
+
+		resp, err := NewUnauthenticatedReq("POST", apiUrl+"/sessions/refresh", body)
+
+		require.NoError(t, err, "Failed on make a refresh validate request")
+
+		bodyBytes, err := io.ReadAll(resp.Body)
+
+		require.NoError(t, err, "Failed to parse refresh validate body response")
+
+		defer resp.Body.Close()
+
+		err = ParseBody(bodyBytes, &baseResponseRefresh)
+
+		require.NoError(t, err, "Failed to parse refresh validate response body")
+
+		assert.Equal(t, resp.StatusCode, http.StatusUnauthorized)
+		assert.False(t, baseResponseRefresh.Success)
+		assert.Contains(t, baseResponseRefresh.Message, "sessão inválida ou expirada")
+	})
+
+	t.Run("should logout successful", func(t *testing.T) {
+		var loginData handlers.LoginRequest
+		var baseResponse response.BaseResponse[session.LoginOutput]
+		var baseResponseLogout response.BaseResponse[any]
+		var logoutData handlers.RefreshRequest
+
+		loginData.Email = cfg.AdminUser.Email
+		loginData.Password = cfg.AdminUser.Password
+
+		bodyBytes, resp := loginRequest(t, loginData, apiUrl)
+
+		err = ParseBody(bodyBytes, &baseResponse)
+
+		require.NoError(t, err, "Failed to parse response body")
+
+		assert.Equal(t, resp.StatusCode, http.StatusOK)
+		assert.True(t, baseResponse.Success)
+		assert.NotEmpty(t, baseResponse.Data.RefreshToken)
+
+		logoutData.RefreshToken = baseResponse.Data.RefreshToken
+
+		payload, err := BuildBody(logoutData)
+
+		require.NoError(t, err, "Failed to build logout request")
+
+		resp, err = NewUnauthenticatedReq("DELETE", apiUrl+"/sessions/logout", payload)
+
+		require.NoError(t, err, "Failed on make a logout request")
+
+		bodyBytes, err = io.ReadAll(resp.Body)
+
+		require.NoError(t, err, "Failed to parse logout body response")
+
+		defer resp.Body.Close()
+
+		err = ParseBody(bodyBytes, &baseResponseLogout)
+
+		require.NoError(t, err, "Failed to parse logout response body")
+
+		assert.Equal(t, resp.StatusCode, http.StatusOK)
+		assert.True(t, baseResponseLogout.Success)
+		assert.Equal(t, "Logged out successfully", baseResponseLogout.Data)
+	})
+
+	t.Run("should invalid refresh token return an error on logout", func(t *testing.T) {
+		var baseResponseLogout response.BaseResponse[any]
+		var logoutData handlers.RefreshRequest
+
+		logoutData.RefreshToken = "invalidtoken"
+
+		payload, err := BuildBody(logoutData)
+
+		require.NoError(t, err, "Failed to build logout request")
+
+		resp, err := NewUnauthenticatedReq("DELETE", apiUrl+"/sessions/logout", payload)
+
+		require.NoError(t, err, "Failed on make a logout request")
+
+		bodyBytes, err := io.ReadAll(resp.Body)
+
+		require.NoError(t, err, "Failed to parse logout body response")
+
+		defer resp.Body.Close()
+
+		err = ParseBody(bodyBytes, &baseResponseLogout)
+
+		require.NoError(t, err, "Failed to parse logout response body")
+
+		assert.Equal(t, http.StatusUnauthorized, resp.StatusCode)
+		assert.False(t, baseResponseLogout.Success)
+		assert.Contains(t, baseResponseLogout.Message, "sessão inválida ou expirada")
+	})
 }
 
 func loginRequest(t *testing.T, loginData handlers.LoginRequest, apiUrl string) ([]byte, *http.Response) {
