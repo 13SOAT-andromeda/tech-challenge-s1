bf07f888cb7763304bd5846ee2138b949e8a81b0
Juliovaz <julio.oliveiravaz0@gmail.com>
2026-01-01 23:00:26 -0300

    Feature/PHF-99 Base Terraform Config (#43)

---
+++ infra/variables.tf
@@ -0,0 +1,29 @@
+variable "cluster_name" {
+  type        = string
+  default     = "tech-challenge-api"
+  description = "The name of the project"
+}
+
+variable "environment" {
+  type        = string
+  default     = "dev"
+  description = "The deployment environment (e.g., dev, staging, prod)"
+}
+
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+  description = "The AWS region to deploy resources in"
+}
+
+variable "instance_name" {
+  type        = string
+  default     = "tech-challenge-api-node-group"
+  description = "The name of the EC2 instance"
+}
+
+variable "instance_types" {
+  type        = list(string)
+  default     = ["t4g.small", "t4g.medium"]
+  description = "The type of EC2 instance"
+}
